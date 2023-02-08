d51ee765331ea6bf7396e9f1ca5e101712f9f7d8
Lukas Forer <lukas.forer@i-med.ac.at>
2023-02-08 10:36:23 +0100

    Prepare release 0.1.0

--- src/main/java/com/github/lukfor/App.java
+++ src/main/java/com/github/lukfor/App.java
@@ -12,7 +12,7 @@ public class App {
 
 	public static final String APP = "tabix-merge";
 
-	public static final String VERSION = "0.0.1";
+	public static final String VERSION = "0.1.0";
 
 	public static final String URL = "https://github.com/lukfor/tabix-merge";
 
