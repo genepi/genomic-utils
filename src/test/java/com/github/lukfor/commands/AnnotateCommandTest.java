5194cfbe532a706bc63913fc58faa516a219a461
Lukas Forer <lukas.forer@i-med.ac.at>
2023-03-16 11:35:07 +0100

    Add simple testcase for annotate command

---
+++ src/test/java/com/github/lukfor/commands/AnnotateCommandTest.java
@@ -0,0 +1,102 @@
+package com.github.lukfor.commands;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.github.lukfor.io.AnnotationMatchingStrategy;
+
+public class AnnotateCommandTest {
+
+	@Test
+	public void testRegenieGz() throws Exception {
+
+		AnnotateCommand command = new AnnotateCommand();
+		command.setInput("test-data/Y2.regenie.gz");
+		command.setChr("CHROM");
+		command.setPosition("GENPOS");
+		command.setAnno("test-data/rsids.tsv.gz");
+		command.setAnnoColumns("RSID,REF,ALT");
+		command.setStrategy(AnnotationMatchingStrategy.CHROM_POS_ALLELES);
+		command.setRef("ALLELE0");
+		command.setAlt("ALLELE1");
+
+		command.setOutput(createOutputFile());
+		assertEquals(0, (int) command.call());
+		assertEquals(1000, command.getTotalLines());
+		assertEquals(1000, command.getAnnotatedLines());
+	}
+
+	@Test
+	public void testRegenieTxt() throws Exception {
+
+		AnnotateCommand command = new AnnotateCommand();
+		command.setInput("test-data/Y2.regenie.txt");
+		command.setChr("CHROM");
+		command.setPosition("GENPOS");
+		command.setAnno("test-data/rsids.tsv.gz");
+		command.setAnnoColumns("RSID,REF,ALT");
+		command.setStrategy(AnnotationMatchingStrategy.CHROM_POS_ALLELES);
+		command.setRef("ALLELE0");
+		command.setAlt("ALLELE1");
+
+		command.setOutput(createOutputFile());
+		assertEquals(0, (int) command.call());
+		assertEquals(1000, command.getTotalLines());
+		assertEquals(1000, command.getAnnotatedLines());
+	}
+
+	@Test
+	public void testTestDataWithNoAlleleSwitches() throws Exception {
+
+		AnnotateCommand command = new AnnotateCommand();
+		command.setInput("test-data/Y2.regenie.txt");
+		command.setChr("CHROM");
+		command.setPosition("GENPOS");
+		command.setAnno("test-data/rsids.tsv.gz");
+		command.setAnnoColumns("RSID,REF,ALT");
+		command.setStrategy(AnnotationMatchingStrategy.CHROM_POS_ALLELES_EXACT);
+		command.setRef("ALLELE0");
+		command.setAlt("ALLELE1");
+
+		command.setOutput(createOutputFile());
+		assertEquals(0, (int) command.call());
+		assertEquals(1000, command.getTotalLines());
+		assertEquals(995, command.getAnnotatedLines());
+	}
+
+	@Test
+	public void testTestDataWithoutAlleles() throws Exception {
+
+		AnnotateCommand command = new AnnotateCommand();
+		command.setInput("test-data/Y2.regenie.txt");
+		command.setChr("CHROM");
+		command.setPosition("GENPOS");
+		command.setAnno("test-data/rsids.tsv.gz");
+		command.setAnnoColumns("RSID,REF,ALT");
+		command.setStrategy(AnnotationMatchingStrategy.CHROM_POS);
+		command.setRef("ALLELE0");
+		command.setAlt("ALLELE1");
+
+		command.setOutput(createOutputFile());
+		assertEquals(0, (int) command.call());
+		assertEquals(1000, command.getTotalLines());
+		// one snp hast multiple entries in index (multiallelic)
+		assertEquals(999, command.getAnnotatedLines());
+	}
+
+	protected String createOutputFile() {
+		try {
+			File tempFile = File.createTempFile("output", ".txt");
+			tempFile.deleteOnExit();
+			return tempFile.getAbsolutePath();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "";
+		}
+	}
+
+}
