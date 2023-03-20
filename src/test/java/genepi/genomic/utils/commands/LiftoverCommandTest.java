8c7f0c81c06e1f2b92ec70defd94aa1159d1cfa2
seppinho <sebastian.schoenherr@i-med.ac.at>
2023-03-20 21:11:56 +0100

    Add liftover testcase for specific gene

--- src/test/java/genepi/genomic/utils/commands/LiftoverCommandTest.java
+++ src/test/java/genepi/genomic/utils/commands/LiftoverCommandTest.java
@@ -28,7 +28,7 @@ public class LiftoverCommandTest {
 		assertEquals(6, command.getFlipped());
 
 	}
-	
+
 	@Test
 	public void testLiftHg38ToHg19() throws Exception {
 
@@ -43,11 +43,38 @@ public class LiftoverCommandTest {
 		assertEquals(0, (int) command.call());
 
 		assertEquals(8, command.getTotal());
-		//TODO:  check: 6 snps on chr22 are after lifotver on chr 14? 
+		// TODO: check: 6 snps on chr22 are after lifotver on chr 14?
 		assertEquals(2, command.getResolved());
 		assertEquals(6, command.getFailed());
 		assertEquals(0, command.getFlipped());
 
+	}
+
+	@Test
+	public void testLiftHg19ToHg38ToHg19() throws Exception {
+
+		LiftoverCommand command = new LiftoverCommand();
+		command.setInput("test-data/lpa.regenie_hg19.gz");
+		command.setChr("CHROM");
+		command.setPosition("GENPOS");
+		command.setAlt("ALLELE1");
+		command.setRef("ALLELE0");
+		command.setChainFile("files/chains/hg19ToHg38.over.chain.gz");
+		command.setSnpId("ID");
+		command.setUpdateId(true);
+		command.setOutput("lpa.regenie_hg38.txt");
+		assertEquals(0, (int) command.call());
+		assertEquals(376, command.getResolved());
+		assertEquals(0, command.getFailed());
+		assertEquals(0, command.getFlipped());
+		assertEquals(376, command.getTotal());
+		
+		command.setInput("lpa.regenie_hg38.txt");
+		command.setChainFile("files/chains/hg38ToHg19.over.chain.gz");
+		command.setOutput("lpa.regenie_hg19.txt");
+		assertEquals(0, (int) command.call());
+		assertEquals(0, command.getFailed());
+		
 	}
 
 	protected String createOutputFile() {
