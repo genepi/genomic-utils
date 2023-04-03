1fd0646aff0f9e9ae2b8c917ce66f124b6c441a8
Lukas Forer <lukas.forer@i-med.ac.at>
2023-04-03 13:55:38 +0200

    Prepare release 0.1.2

--- src/main/java/genepi/genomic/utils/App.java
+++ src/main/java/genepi/genomic/utils/App.java
@@ -13,7 +13,7 @@ public class App {
 
 	public static final String APP = "genomic-utils";
 
-	public static final String VERSION = "0.1.1";
+	public static final String VERSION = "0.1.2";
 
 	public static final String URL = "https://github.com/genepi/genomic-utils";
 
