5a6e040f7adee0032a94a15fbefc2bd29714422d
Lukas Forer <lukas.forer@i-med.ac.at>
2024-03-14 13:08:54 +0100

    Add missing changes

--- src/main/java/genepi/genomic/utils/commands/csv/CsvToBedCommand.java
+++ src/main/java/genepi/genomic/utils/commands/csv/CsvToBedCommand.java
@@ -2,6 +2,8 @@ package genepi.genomic.utils.commands.csv;
 
 import java.util.concurrent.Callable;
 
+import genepi.genomic.utils.commands.csv.region.DbSnpIndex;
+import genepi.genomic.utils.commands.csv.region.GeneIndex;
 import genepi.genomic.utils.commands.csv.region.GenomicRegion;
 import genepi.io.text.LineReader;
 import genepi.io.text.LineWriter;
@@ -17,6 +19,15 @@ public class CsvToBedCommand implements Callable<Integer> {
 	@Option(names = "--output", description = "output bed file", required = true)
 	private String output;
 
+	@Option(names = "--build", description = "genome build", required = true)
+	private String build = "hg19";
+
+	@Option(names = "--genes", description = "gene index file", required = false)
+	private String genes = null;
+
+	@Option(names = "--dbsnp", description = "dbsnp index file", required = false)
+	private String dbsnp = null;
+
 	public void setInput(String input) {
 		this.input = input;
 	}
@@ -25,19 +36,44 @@ public class CsvToBedCommand implements Callable<Integer> {
 		this.output = output;
 	}
 
+	public void setGenes(String genes) {
+		this.genes = genes;
+	}
+
+	public void setBuild(String build) {
+		this.build = build;
+	}
+
+	public void setDbSnp(String dbsnp) {
+		this.dbsnp = dbsnp;
+	}
+
 	@Override
 	public Integer call() throws Exception {
 
 		assert (input != null);
 		assert (output != null);
 
-		LineReader reader = new LineReader(input);
+		if (genes != null) {
+			GeneIndex.load(genes, build);
+		}
+
+		if (dbsnp != null) {
+			DbSnpIndex.setFilename(dbsnp);
+		}
+
 		LineWriter writer = new LineWriter(output);
+		LineReader reader = new LineReader(input);
 
 		while (reader.next()) {
 			String line = reader.get();
-			GenomicRegion region = new GenomicRegion().parse(line);
-			writer.write(region.toBedFormat());
+			try {
+				GenomicRegion region = GenomicRegion.parse(line.trim(), build);
+				writer.write(region.toBedFormat());
+			} catch (Exception e) {
+				System.out.println("Error processing query: '" + line + "': " + e.getMessage());
+				return 1;
+			}
 		}
 
 		reader.close();
